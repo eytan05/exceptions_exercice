1f124b7b6e5ccbd677adb5bbabb14a028dd55dbe
eytan05 <eytan.fredj-zadoun@epita.fr>
2023-10-02 15:07:50 +0200

    exercice 2.2

---
+++ db_connection.py
@@ -0,0 +1,23 @@
+from psycopg2 import OperationalError
+import psycopg2
+
+
+def connect_to_db(env):
+    connection = None
+    cursor = None
+    try:
+        connection = psycopg2.connect(database=env["database"], user=env["username"],
+                                       password=env["password"], host=env["host"], port=env["port"])
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM MovieInfo;")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except OperationalError as e:
+        print(f"L'erreur '{e}' s'est produite. La base de données n'est pas accessible.")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+            print("the connection to Postgresql is closed")
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import os
+from db_connection import connect_to_db
+from dotenv import load_dotenv
+
+
+def get_env_val():
+    load_dotenv(".env")
+    return {
+        "username": os.getenv('DB_USER'),
+        "password": os.getenv("DB_PASSWORD"),
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
+        "database": os.getenv("DB_NAME"),
+    }
+
 
 if __name__ == '__main__':
-    print("Hello ! this is the branch master")
+    env_var = get_env_val()
+    connect_to_db(env_var)
